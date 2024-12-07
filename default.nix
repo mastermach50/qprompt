c4451d19c94f3d8070e4fe6f8654daea5b188ead
Mathew Manoj (root) <64970593+mastermach50@users.noreply.github.com>
2024-12-08 01:53:06 +0530

    removed ./result

--- default.nix
+++ default.nix
@@ -1,16 +1,31 @@
-{ pkgs ? import <nixpkgs> {},
-}:
-
-pkgs.rustPlatform.buildRustPackage {
+{ pkgs ? import <nixpkgs> }:
+let
+  lib = pkgs.lib;
+  rustPlatform = pkgs.rustPlatform;
+  fetchFromGitHub = pkgs.fetchFromGitHub;
+  libxkbcommon = pkgs.libxkbcommon;
+  wayland = pkgs.wayland;
+in
+rustPlatform.buildRustPackage rec {
   pname = "qprompt";
   version = "0.1.1";
-  src = pkgs.lib.cleanSource ./.;
+  
+  src = fetchFromGitHub {
+    owner = "mastermach50";
+    repo = "qprompt";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-xAXXsb7H7gU0v2Zm0bgo1sQJ8SLB86iwl0j2TcnOaAA=";
+  };
 
-  useFetchCargoVendor = true;
   cargoHash = "sha256-J29jP6tCkyOj+Le2d5nteGp25Br9OVi1GlvYpuVR/dE=";
-
+  
   postFixup = ''
     patchelf $out/bin/qprompt \
-      --add-rpath ${pkgs.lib.makeLibraryPath [ pkgs.libxkbcommon pkgs.wayland ]}
+      --add-rpath ${lib.makeLibraryPath [ libxkbcommon wayland ]}
   '';
+
+  meta = {
+    description = "A GUI app to fetch input from user, for use in scripts";
+    platforms = [ "x86_64-linux" ];
+  };
 }
